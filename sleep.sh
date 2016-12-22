4bf9bdaf2439d453c711cbeb881b4c1a3b4b3995
Brian <bken@dz.ior>
2016-12-22 18:32:07 +0000

    sleepy

---
+++ sleep.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+for i in {99..1}
+do
+  echo "$i bottles of beer on the wall, $i bottles of beer, take one down, pass it around, $(($i-1)) bottles of beer on the wall";
+  sleep 1
+done
+
+echo ''
+echo '         .   *   ..  . *  *'
+echo '       *  * @()Ooc()*   o  .'
+echo '           (Q@*0CG*O()  ___'
+echo '          |\_________/|/ _ \'
+echo '          |  |  |  |  | / | |'
+echo '          |  |  |  |  | | | |'
+echo '          |  |  |  |  | | | |'
+echo '          |  |  |  |  | | | |'
+echo '          |  |  |  |  | | | |'
+echo '          |  |  |  |  | \_| |'
+echo '          |  |  |  |  |\___/'
+echo '          |\_|__|__|_/|'
+echo '           \_________/'
+echo ''
+echo '           all drunk!'
+
